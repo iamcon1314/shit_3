ef0fe830190284a5b0dd85f67aa1ea280de777a2
iamcon1314 <lprlpr666@outlook.com>
2023-08-27 22:50:21 +0800

    1

---
+++ src/ast/exprnode/debug.java
@@ -0,0 +1,16 @@
+package src.ast.exprnode;
+
+import static java.lang.Thread.sleep;
+
+public class debug {
+    public void debug1(){
+        try {
+            sleep(400);byte[] memoryChunk = new byte[1024 * 1024];byte[] memoryChunk1 = new byte[1024 * 1024];
+        } catch (InterruptedException e) {
+
+        }
+    }
+    public void debug2(){
+
+    }
+}
